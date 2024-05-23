70439f0406d64bc71c6b7af19480bc1546364910
Lewis Mwangi Wambugu <wambugulewis20@gmail.com>
2024-05-23 10:08:52 +0300

    add

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,17 @@
 def write_file(file_name, file_content):
-    pass
+    """Write the file_content to a .txt file with the given file_name."""
+    full_file_name = f"{file_name}.txt"
+    with open(full_file_name, 'w') as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    """Append the append_content to a .txt file with the given file_name."""
+    full_file_name = f"{file_name}.txt"
+    with open(full_file_name, 'a') as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    """Read the content of a .txt file with the given file_name and return it."""
+    full_file_name = f"{file_name}.txt"
+    with open(full_file_name, 'r') as file:
+        return file.read()
